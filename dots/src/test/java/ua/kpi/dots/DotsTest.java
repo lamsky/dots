b2fe66b1b80cc9ae5fee7aed14a064e31ea33c6a
OblamskyiV <vlad.oblamsky@gmail.com>
2013-03-24 20:25:59 +0200

    Initial test cases

---
+++ dots/src/test/java/ua/kpi/dots/DotsTest.java
@@ -0,0 +1,16 @@
+package ua.kpi.dots;
+
+public class DotsTest {
+
+    // We have a field
+    // Field has some size
+    // We have 2 players
+    // Players take turns
+    // Dots can be 2 types = from 2 players
+    // Dots can be placed on the field
+    // When there is a loop, which consists of same type dots, it's become closed
+    // All enemy dots in closed loop are captured
+    // Loops can't be intersected
+    // Game is over when there is no place to put dot outside loops
+
+}
