a43321c1c2bb9d5b0f0622b9a549aed4b5d78981
Misha <KOTSYURUBA@GMAIL.COM>
2013-03-25 16:51:56 +0200

    Convention

--- dots/src/test/java/ua/kpi/dots/DotsTest.java
+++ dots/src/test/java/ua/kpi/dots/DotsTest.java
@@ -16,43 +16,47 @@ public class DotsTest {
 
     // We have a field
     @Test
-    public void shouldExistFieldWhenGameCreated() {
+    public void shouldExistField_WhenGameCreated() {
         new Field(1);
     }
 
     // Field has some size
     @Test
-    public void shouldFieldHasSizeWhenCreated() {
+    public void shouldFieldHasSize_WhenCreated() {
         Field field = new Field(1);
 
         assertEquals(1, field.getSize());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenSizeIsNegative() {
+    public void shouldThrowException_WhenSizeIsNegative() {
         new Field(-1);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenSizeIsZero() {
+    public void shouldThrowException_WhenSizeIsZero() {
         new Field(0);
     }
 
     // We have 2 players
     @Test
-    public void shouldExistRedPlayerWhenGameCreates() {
+    public void shouldExistRedPlayer_WhenGameCreates() {
         assertNotNull(dots.getRedPlayer());
     }
 
     @Test
-    public void shouldExistBluePlayerWhenGameCreates() {
+    public void shouldExistBluePlayer_WhenGameCreates() {
         assertNotNull(dots.getBluePlayer());
     }
+
+
+
 
     // Players take turns
     // Dots can be 2 types = from 2 players
     // Dots can be placed on the field
-    // When there is a loop, which consists of same type dots, it's become closed
+    // When there is a loop, which consists of same type dots,
+    //   it's become closed
     // All enemy dots in closed loop are captured
     // Loops can't be intersected
     // Game is over when there is no place to put dot outside loops
